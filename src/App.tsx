daf9494649cc1e37a650b63e6e915bfa47b72b6b
Sandro Soria <sandro33810@gmail.com>
2024-07-07 15:55:58 +0200

    Mettre à jour la page d'accueil avec un message différent

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
+import { useState } from "react";
+import { invoke } from "@tauri-apps/api/tauri";
+
 function App() {
+  const [greetMsg, setGreetMsg] = useState("");
+  const [name, setName] = useState("");
+
+  async function greet() {
+    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+    setGreetMsg(await invoke("greet", { name }));
+  }
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Si tu viens de tomber sur cette application tu es considéré comme un <strong>noob</strong> en développement web.
-        </p>
-      </header>
+    <div>
+      <p>Si tu viens de tomber sur cette page tu es alors suspecté(e) alors d'être un espion</p>
     </div>
   );
 }
