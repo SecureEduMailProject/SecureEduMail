f16b015ee2284ff0f8cf91ef00511f482378aced
Sandro Soria <sandro33810@gmail.com>
2024-07-07 15:59:14 +0200

    Mettre à jour le message d'accueil dans App.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <p>Si tu es tombé sur cette application alors tu es suspecté(e) d'être un(e) espion</p>
   </React.StrictMode>,
 );
