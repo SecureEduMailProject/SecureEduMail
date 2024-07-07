51279906b877e18939362699c1528d7fae7937e1
Sandro Soria <sandro33810@gmail.com>
2024-07-07 15:56:29 +0200

    Mettre à jour la fonction greet dans App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,6 @@ import { useState } from "react";
 import { invoke } from "@tauri-apps/api/tauri";
 
 function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
-
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    setGreetMsg(await invoke("greet", { name }));
-  }
 
   return (
     <div>
