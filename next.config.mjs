190212d83713a45c9473b79c5dc6ba65a6726b0d
Sandro Soria <sandro33810@gmail.com>
2024-07-15 14:40:00 +0200

    final commit de src-tauri

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    distDir: 'build', // Changez 'build' par le répertoire de votre choix
+};
 
 export default nextConfig;
